bb65b27b33fac110a710a9d3f0b65a1e31224d75
LobanovVIK <tel0504003118@gmail.com>
2026-02-04 02:06:39 +0200

    Initial Commit

---
+++ PingMaster/PingMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingMasterApp.swift
+//  PingMaster
+//
+//  Created by Lobanov Viktor on 04.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct PingMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
